2c8f4e2f33fe9f9240e4492aff4f67d522bb699c
Shivam Dwivedi <shivamdwivedicse20919@gmail.com>
2025-11-16 21:26:31 +0530

    Add files via upload
    🚀 Built a simple Python Weather App using the OpenWeather API!
    Learned how APIs work and how to fetch real-time data in Python.
    Excited to explore more API-based projects! 🌦️💻

---
+++ weather.py
@@ -0,0 +1,19 @@
+import requests
+
+API_KEY = "Your_api_key"
+city = input("Enter city name: ")
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+if data["cod"] == 200:
+    print("City:", data["name"])
+    print("Temperature:", data["main"]["temp"], "°C")
+    print("Humidity:", data["main"]["humidity"], "%")
+    print("Weather Condition:", data["weather"][0]["description"])
+else:
+    print("City not found! Please enter a valid name.")
+
+
